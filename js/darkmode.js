87232a60d43dfc5a39b680c4e2ff0b1e62f8b5c6
ayn96 <ayn96ac.ca@gmail.com>
2024-03-11 16:05:04 -0700

    created modal

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,7 +1,17 @@
 // dark mode with Jquery
-$(document).ready(function() {
-  const darkMode = () => {
-    let darkModeEnabled = localStorage.getItem("darkModeEnabled") == "true";  //true = darkmode on
+const darkMode = () => {
+  let darkModeEnabled = localStorage.getItem("darkModeEnabled") == "true";  //true = darkmode on
+
+  if (darkModeEnabled) {
+    $("body").addClass("dark-mode");
+    $(".mode_switch").text("Light Mode"); 
+  } else {
+    $("body").removeClass("dark-mode"); 
+    $(".mode_switch").text("Dark Mode"); 
+  }
+
+  $(".mode_switch").click(() => {
+    darkModeEnabled = !darkModeEnabled; //toggle
 
     if (darkModeEnabled) {
       $("body").addClass("dark-mode");
@@ -11,20 +21,8 @@ $(document).ready(function() {
       $(".mode_switch").text("Dark Mode"); 
     }
 
-    $(".mode_switch").click(() => {
-      darkModeEnabled = !darkModeEnabled; //toggle
+    localStorage.setItem('darkModeEnabled', darkModeEnabled);
+  });
+};
 
-      if (darkModeEnabled) {
-        $("body").addClass("dark-mode");
-        $(".mode_switch").text("Light Mode"); 
-      } else {
-        $("body").removeClass("dark-mode"); 
-        $(".mode_switch").text("Dark Mode"); 
-      }
-
-      localStorage.setItem('darkModeEnabled', darkModeEnabled);
-    });
-  };
-
-  darkMode();
-});
+darkMode();
